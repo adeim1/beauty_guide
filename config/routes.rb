145a236bd80576f5c155e02ee8817a795d7ec5e1
Adeim Ubakeeva <adeim5d.o@gmail.com>
2022-12-05 21:40:33 +0600

    Created 6 routines

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   get "/about", to: "pages#about"
-  # resources :routines do
-  #   collection do
-  #     get :top
-  #   end
-  # end
   get "/routines/:id", to: "routines#show"
 end
 
-# resourses
