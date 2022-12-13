7a1d3c89e09e70fc9c945aedea0d383359bc037b
Adeim Ubakeeva <adeim5d.o@gmail.com>
2022-12-13 14:46:20 +0600

    fixed some front-end of home page and background photo

--- app/controllers/routines_controller.rb
+++ app/controllers/routines_controller.rb
@@ -1,18 +1,4 @@
 class RoutinesController < ApplicationController
-  # attr_reader :oily, :combi, :dry, :aging, :acne, :pigmentation, :sensitive, :darker
-
-  # def initialize(oily, combi, dry, aging, acne, pigmentation, sensitive, darker)
-  #   @oily = oily
-  #   @combi = combi
-  #   @dry = dry
-  #   @aging = aging
-  #   @acne = acne
-  #   @pigmentation = pigmentation
-  #   @sensitive = sensitive
-  #   # @oilliness = oilliness
-  #   @darker = darker
-  # end
-
   def show
     @routine = Routine.find(params[:id])
     @routine_product = RoutineProduct.new
--- app/controllers/user_routines_controller.rb
+++ app/controllers/user_routines_controller.rb
@@ -3,3 +3,13 @@ class UserRoutinesController < ApplicationController
     @user_routine = UserRoutine.find(params[:id])
   end
 end
+
+# @recipe = Recipe.find(params[:recipe_id])
+#     @favourite = Favourite.new(recipe_id: params[:recipe_id])
+#     @favourite.user_id = current_user.id
+#     if @favourite.save
+#      redirect_to produce_recipes_path(params[:produce_id])
+#     else
+#      render :create
+#     end
+#   end
